3a9353127c1b191eb3c32f9bbcc16c860b8ac954
Elleni Bisrat <ellenibisrat16@gmail.com>
2025-05-16 21:43:08 +0300

    Update config.php

--- backend/config.php
+++ backend/config.php
@@ -1,8 +1,15 @@
 <?php
-header("Access-Control-Allow-Origin: *");
-header("Content-Type: application/json; charset=UTF-8");
-header("Access-Control-Allow-Methods: GET, POST, DELETE");
+$origin = "https://addandremove.vercel.app";
+header("Access-Control-Allow-Origin: $origin");
+header("Access-Control-Allow-Methods: GET, POST, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+header("Content-Type: application/json; charset=UTF-8");
+
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(204);
+    exit();
+}
 
 $servername = "sql305.infinityfree.com";
 $username = "if0_38994351";
@@ -17,5 +24,6 @@ try {
     exit();
 }
 ?>
+
 
 
